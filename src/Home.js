23791a01e68ce4658022af74685d1875c8b24142
tayyab.shabab <tayyab.shabab@cinnober.com>
2017-06-26 16:07:26 +0200

    First Commit

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import AnimatedWrapper from "./AnimatedWrapper";
+
+class HomeComponent extends Component {
+   render() {
+      return (
+         <div className="page">
+            <h1>Home</h1>
+            <p>Hello from the home page!</p>
+         </div>
+      )
+   }
+}
+
+const Home = AnimatedWrapper(HomeComponent);
+export default Home;
---
+++ src/Subpage.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import AnimatedWrapper from "./AnimatedWrapper";
+
+class SubpageComponent extends Component {
+   render() {
+      return (
+         <div className="page">
+            <h1>Subpage</h1>
+            <p>Hello from a sub page!</p>
+         </div>
+      )
+   }
+}
+
+const Subpage = AnimatedWrapper(SubpageComponent);
+export default Subpage;
